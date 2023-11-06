0dba1095e98e17ac2a94c0238f48401b296fdbe4
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-11-06 14:47:35 -0600

    add questions and link markdown module

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?', 
+    'Please provide a brief description of your project:',
+    'Please enter any specific installation instructions required for this project:',
+    'Please enter any specific usage information pertinent to this project:', 
+    'Please provide any guidelines for contributing to this project:',
+    'Please provide any instructions for testing this application:',
+    'Please list your GitHub username for any follow-up questions:',
+    'Please provide an email address:'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+    .prompt([
+        {
+        type: 'input',
+        message: questions[0],
+        name: 'title',
+        },
+        {
+        type: 'input',
+        message: questions[1],
+        name: 'description',
+        },
+        {
+        type: 'input',
+        message: questions[2],
+        name: 'install',
+        },
+        {
+        type: 'input',
+        message: questions[3],
+        name: 'usage',
+        },
+        {
+        type: 'input',
+        message: questions[4],
+        name: 'contribute',
+        },
+        {
+        type: 'input',
+        message: questions[5],
+        name: 'testing',
+        },
+        {
+        type: 'input',
+        message:questions[6],
+        name: 'gitHub',
+        },
+        {
+        type: 'input',
+        message: questions[7],
+        name: 'email',
+        },
+                        
+    ])
+    .then(markdown.generateMarkdown(response)
+    );
+
+}
 
 // Function call to initialize app
 init();
